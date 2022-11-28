f80bebd09d116cc7b340e1723c092115af405898
mmaxwu <mmaxwu@users.noreply.github.com>
2022-11-28 19:21:24 +0000

    deploy: 8d0e838eb38e5691f346140b0778f15c00024f60

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/cs/assets/js/search-data.json', true);
+    request.open('GET', '/Tri2-GroupFastpages/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
